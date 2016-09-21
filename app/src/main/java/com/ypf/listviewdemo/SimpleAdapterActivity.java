a9be10c1e6a9dc30183c762f069544f7bf6a4d65
Elso <284141786@qq.com>
2016-09-21 18:18:36 +0800

    修复SimpleAdapter不能显示的问题

--- app/src/main/java/com/ypf/listviewdemo/SimpleAdapterActivity.java
+++ app/src/main/java/com/ypf/listviewdemo/SimpleAdapterActivity.java
@@ -14,17 +14,19 @@ import java.util.List;
  */
 public class SimpleAdapterActivity extends Activity {
     private ListView simpleAdapterView;
+
+    int images[] = {R.mipmap.rocket, R.mipmap.rocket, R.mipmap.rocket, R.mipmap.rocket, R.mipmap.rocket};
+    List<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
+
+    String[] from = new String[]{"itemImage", "itemTitle", "itemText"}; //与动态数组中的Key对应
+    int[] to = new int[]{R.id.itemImage, R.id.itemTitle, R.id.itemText};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simpleadapter);
 
         simpleAdapterView = (ListView)findViewById(R.id.listView2);
-        int images[] = {R.mipmap.rocket, R.mipmap.rocket, R.mipmap.rocket, R.mipmap.rocket, R.mipmap.rocket};
-        List<HashMap<String, Object>> data = new ArrayList<HashMap<String, Object>>();
-
-        String[] from = new String[]{"itemImage", "itemTitle", "itemText"};
-        int[] to = new int[]{R.id.itemImage, R.id.itemTitle, R.id.itemText};
 
         //创建动态数据
         for(int i=0; i<5; i++){
@@ -35,7 +37,8 @@ public class SimpleAdapterActivity extends Activity {
             data.add(map);
         }
 
-        SimpleAdapter adapter = new SimpleAdapter(this, data, R.layout.activity_simpleadapter, from, to);
+        //从动态数据data中，取出form数组Key对应的value的值，填充到to数组中对应的控件中
+        SimpleAdapter adapter = new SimpleAdapter(this, data, R.layout.simpleadapter_item, from, to);
         simpleAdapterView.setAdapter(adapter);
     }
 }
